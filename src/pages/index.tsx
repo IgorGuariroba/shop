4f9a56d2bcfcbb03006ceaa8815daccc8f7c0fc8
igor.guariroba <igorguariroba@movida.com.br>
2022-10-29 17:39:52 -0300

    styled carregado do lado do serviddor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import { styled } from "../styles"
+
+const Button = styled('button',{
+  backgroundColor: 'Purple',
+  color: 'white',
+  border: 0,
+  padding: '0.5rem 1rem',
+})
+
+
 export default function Home() {
   return (
-    <h1>oie</h1>
+    <Button>Enviar</Button>
   )
 }
