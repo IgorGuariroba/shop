3fb3a4d543397f9f09202c489f53a6ba9763fb8d
igor.guariroba <igorguariroba@movida.com.br>
2022-10-30 16:07:05 -0300

    Stilo global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
+import { AppProps } from "next/app"
+import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image 
+        src={logoImg.src} 
+        alt=""  
+        width={48}
+        height={48}
+        />
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -10,7 +10,14 @@ createTheme
 } = createStitches({
   theme: {
     colors: {
-      red: '#ff0000',
+      white: '#fff',
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
     }
   }
 })
