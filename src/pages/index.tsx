b23974c15534ef32193b33061d68a95df29b1738
igor.guariroba <igorguariroba@movida.com.br>
2022-11-03 15:09:32 -0300

    Utilizando o getStaticProps do next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
-import camiseta from '../assets/1.png'
-import camiseta2 from '../assets/2.png'
-import camiseta3 from '../assets/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -47,7 +44,7 @@ export default function Home({ products }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -66,6 +63,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2,
   }
 }
