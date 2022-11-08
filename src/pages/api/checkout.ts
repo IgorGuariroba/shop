617177e7f57852d688bd01c8fb9566a4faf786d1
igor.guariroba <igorguariroba@movida.com.br>
2022-11-08 17:36:33 -0300

    Permitir que apenas metodos post sejam aceitos

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -2,23 +2,31 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const priceId = 'price_1LzVmxB8aW0cpkCBnwLD9E5l'
+  const { priceId } = req.body
+
+  if (req.method === 'POST') {
+    return res.status(405)
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' })
+  }
 
   const successUrl = `${process.env.NEXT_URL}/success`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
-    cancel_url: cancelUrl,   
-      mode: 'payment',
-      line_items: [
-        {
-          price: priceId,
-          quantity: 1,
-        }
-      ],
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      }
+    ],
   })
-    return res.status(201).json({
-      checkoutUrl: checkoutSession.url,
-    })
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
 }
